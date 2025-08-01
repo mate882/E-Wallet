3bb27f28bb2bbb3d9ae860daaa58299427c95364
mate882 <mateasanidze808@gmail.com>
2025-08-01 22:15:00 +0400

    Change wsgi.py file

--- EWallet/EWallet/wsgi.py
+++ EWallet/EWallet/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EWallet.EWallet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EWallet.settings')
 
 application = get_wsgi_application()
